f96d659f16288b143feeb70d84d9c9ddaf84918e
ghsumiyasu <ghsumiyasu@gmail.com>
2024-10-29 04:47:40 +0900

    Initial Commit

---
+++ Swift UIKitTests/Swift_UIKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UIKitTests.swift
+//  Swift UIKitTests
+//
+//  Created by Desenvolvimento on 2024/10/29.
+//
+
+import Testing
+@testable import Swift_UIKit
+
+struct Swift_UIKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
