23cd609b26abfae97920f12721e3216be2176a07
ghsumiyasu <ghsumiyasu@gmail.com>
2024-11-04 09:23:40 +0900

    Implementacao UIView Basico

--- Swift UIKit/ViewController.swift
+++ Swift UIKit/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let customView = UIView(frame: .zero)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
     }
-
-
+    private func setupView() {
+        view.backgroundColor = UIColor.red
+        customView.backgroundColor = .black
+        customView.translatesAutoresizingMaskIntoConstraints = false
+        setHierarchy()
+        setConstraints()
+    }
+    private func setHierarchy() {
+        view.addSubview(customView)    }
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            customView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            customView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            customView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
+        ])    }
 }
 
