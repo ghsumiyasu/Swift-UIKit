d775d7c16f00e1fb7689673a7ed1a0e31513f7fc
ghsumiyasu <ghsumiyasu@gmail.com>
2024-11-10 01:16:43 +0900

    Implementacao da TableView

---
+++ Swift UIKit/DailyForecastTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DailyForecastTableViewCell.swift
+//  Swift UIKit
+//
+//  Created by Desenvolvimento on 2024/11/09.
+//
+
+import UIKit
+
+class DailyForecastTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
