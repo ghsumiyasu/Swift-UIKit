4bcb6788f17998fcde94449ffa78d7e80d438ee0
ghsumiyasu <ghsumiyasu@gmail.com>
2024-11-24 13:00:56 +0900

    Fazendo o push da branch UIImageView

--- Swift UIKit/ViewController.swift
+++ Swift UIKit/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let myUIImageView = UIImageView()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        myUIImageView.translatesAutoresizingMaskIntoConstraints = false
+        myUIImageView.image = UIImage(named: "mySun")
+        myUIImageView.contentMode = .scaleAspectFit
+        //
+        view.addSubview(myUIImageView)
+        //
+        NSLayoutConstraint.activate([
+            myUIImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
+            myUIImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            myUIImageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            myUIImageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -300)
+        ])
     }
 
 
