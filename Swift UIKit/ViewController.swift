7754ee0cfefc89f3192ace82235af503b8d6a855
ghsumiyasu <ghsumiyasu@gmail.com>
2024-11-27 04:49:46 +0900

    Alterando o codigo usando o lazy var

--- Swift UIKit/ViewController.swift
+++ Swift UIKit/ViewController.swift
@@ -8,22 +8,81 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //
-    private var myCollectionView = UICollectionView()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //
-        view.backgroundColor = .red
-        //
+    private lazy var myCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         layout.itemSize = CGSize(width: 67, height: 84)
-        layout.sectionInset = UIEdgeInsets(top: .zero, left: 12, bottom: 0, right: 12)
-        let myCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        aula 05 16:10
-        
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 12, bottom: 0, right: 12)
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.backgroundColor = .blue
+        return collection
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        view.addSubview(myCollectionView)
+        NSLayoutConstraint.activate([
+            myCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 300),
+            myCollectionView.heightAnchor.constraint(equalToConstant: 84),
+            myCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            myCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
-
-
 }
+//
+//
+/*
+ class ViewController: UIViewController {
+     //
+     /*
+     private var myCollectionView = UICollectionView()
+     private var layout = UICollectionViewFlowLayout()
+     */
+     //
+     //
+     private lazy var myCollectionView: UICollectionView = {
+         let layout = UICollectionViewFlowLayout()
+         layout.scrollDirection = .horizontal
+         layout.itemSize = CGSize(width: 67, height: 84)
+         layout.sectionInset = UIEdgeInsets(top: 0, left: 12, bottom: 0, right: 12)
+         let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+         collection.translatesAutoresizingMaskIntoConstraints = false
+         collection.backgroundColor = .blue
+         return collection
+     }()
+     //
+     //
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         //
+         view.backgroundColor = .red
+         //
+         
+         
+         /*
+         let layout = UICollectionViewFlowLayout()
+         aula 05 16:00
+         layout.scrollDirection = .horizontal
+         layout.itemSize = CGSize(width: 67, height: 84)
+         layout.sectionInset = UIEdgeInsets(top: .zero, left: 12, bottom: 0, right: 12)
+         myCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+         myCollectionView.translatesAutoresizingMaskIntoConstraints = false
+         myCollectionView.backgroundColor = .blue
+          */
+         //
+         view.addSubview(myCollectionView)
+         //
+         NSLayoutConstraint.activate([
+             myCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 300),
+             myCollectionView.heightAnchor.constraint(equalToConstant: 84),
+             myCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+             myCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+         ])
+     }
 
+
+ }
+
+ */
