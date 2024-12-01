e04ad1442b221d8cd4e91c0f3ba0f1f1ec47619c
ghsumiyasu <ghsumiyasu@gmail.com>
2024-12-01 10:53:14 +0900

    Adcao de cores no Color+Extension

---
+++ Swift UIKit/Color+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Color+Extension.swift
+//  Swift UIKit
+//
+//  Created by Desenvolvimento on 2024/12/01.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let VermelhoBrilhante = UIColor(named: "Vermelho Brilhante")
+}
