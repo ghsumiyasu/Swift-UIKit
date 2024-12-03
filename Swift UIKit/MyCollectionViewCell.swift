5faba4be4413821864e3fff8e715c93f3af39fe1
ghsumiyasu <ghsumiyasu@gmail.com>
2024-12-04 04:56:03 +0900

    Correcao no UICollectionView erro na variavel myCollectionViewCell

--- Swift UIKit/MyCollectionViewCell.swift
+++ Swift UIKit/MyCollectionViewCell.swift
@@ -8,10 +8,101 @@
 import UIKit
 
 class MyCollectionViewCell: UICollectionViewCell {
+
     static let myIndentifier: String = "myCollection"
+
+    private lazy var myStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [myLabel, myImage])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 4
+        stackView.layer.borderWidth = 1
+        stackView.layer.borderColor = UIColor.yellow.cgColor
+        return stackView
+    }()
+    
+    private lazy var myLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Label"
+        label.textColor = .magenta
+        label.font = UIFont.systemFont(ofSize: 10, weight: .semibold)
+        return label
+    }()
+
+    private lazy var myImage: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "myImage")
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    //
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(myStackView)
+        NSLayoutConstraint.activate([
+            myStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            myStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            myStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            myStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            myImage.heightAnchor.constraint(equalToConstant: 33)
+        ])
+        
+        
+        
+    }
+    //Codigo necessario apenas quando usado no storyboard,
+    //Nao e nosso caso, mas mesmo assim o Xcode pede isso
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+
+
+
+/*
+class MyCollectionViewCell: UICollectionViewCell {
+    static let indentifier: String = "myCollection"
+    //
+    private var myStackView = UIStackView()
+    //
+    //
+    private var myLabel = UILabel()
+    private var myImage = UIImageView()
+    //
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.backgroundColor = .green
+        //
+        //
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        myLabel.text = "My Label"
+        myLabel.textColor = .cyan
+        myLabel.font = UIFont.systemFont(ofSize: 10, weight: .semibold)
+        //
+        myImage.translatesAutoresizingMaskIntoConstraints = false
+        myImage.image = UIImage(named: "myImage")
+        myImage.contentMode = .scaleAspectFit
+        //
+        myStackView.translatesAutoresizingMaskIntoConstraints = false
+        myStackView.axis = .vertical
+        myStackView = UIStackView(arrangedSubviews: [myLabel, myImage])
+        //myStackView.spacing = 4
+        myStackView.layer.borderWidth = 1
+        myStackView.layer.borderColor = UIColor.yellow.cgColor
+        //
+        contentView.addSubview(myStackView)
+        //
+        NSLayoutConstraint.activate([
+            myStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            myStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            myStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            myStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+            myImage.heightAnchor.constraint(equalToConstant: 33)
+        ])
     }
     //Codigo necessario apenas quando usado no storyboard,
     //Nao e nosso caso, mas mesmo assim o Xcode pede isso
@@ -19,3 +110,4 @@ class MyCollectionViewCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 }
+*/
--- Swift UIKit/ViewController.swift
+++ Swift UIKit/ViewController.swift
@@ -4,62 +4,20 @@
 //
 //  Created by Desenvolvimento on 2024/10/29.
 //
-
 import UIKit
 
+// Codigo criado usando a variavel lazy que a primeira vista e interessante, mas pretendo estudar mais
+//sobre ela, dando um atencao maior ao modo tradicional de criar variaveis, caso acima ...
 class ViewController: UIViewController, UICollectionViewDataSource {
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt myIndexPath: IndexPath) -> UICollectionViewCell {
-        let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.myIndentifier, for: myIndexPath)
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.myIndentifier, for: indexPath)
         return myCell
     }
-    //
-    private var myLayout = UICollectionViewFlowLayout()
-    //
-    private var myLabel = UILabel()
-    //
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        view.backgroundColor = .red
-        myLayout = UICollectionViewFlowLayout()
-        myLayout.scrollDirection = .horizontal
-        myLayout.itemSize = CGSize(width: 67, height: 84)
-        myLayout.sectionInset = UIEdgeInsets(top: .zero, left: 12, bottom: 0, right: 12)
-        // Uma UICollectionView nao pode ser instanciada com o valor nulo
-        //Necessitando atribuir um valor ja nesse momento
-        let myCollectionView = UICollectionView(frame: .zero, collectionViewLayout: myLayout)
-        myCollectionView.translatesAutoresizingMaskIntoConstraints = false
-        myCollectionView.backgroundColor = .blue
-        myCollectionView.dataSource = self
-        myCollectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: MyCollectionViewCell.myIndentifier)
-        //
-        myLabel.translatesAutoresizingMaskIntoConstraints = false
-        myLabel.text = "My Label"
-        myLabel.textColor = .cyan
-        myLabel.font = UIFont.systemFont(ofSize: 10, weight: .semibold)
-        //
-        view.addSubview(myCollectionView)
-
-        NSLayoutConstraint.activate([
-            myCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 300),
-            myCollectionView.heightAnchor.constraint(equalToConstant: 84),
-            myCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            myCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        ])
-     }
-
-
- }
-
-/*
-// Codigo criado usando a variavel lazy que a primeira vista e interessante, mas pretendo estudar mais
-//sobre ela, dando um atencao maior ao modo tradicional de criar variaveis, caso acima ...
-class ViewController: UIViewController {
+    
     private lazy var myCollectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -68,6 +26,8 @@ class ViewController: UIViewController {
         let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collection.translatesAutoresizingMaskIntoConstraints = false
         collection.backgroundColor = .blue
+        collection.dataSource = self
+        collection.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: MyCollectionViewCell.myIndentifier)
         return collection
     }()
     
@@ -83,4 +43,57 @@ class ViewController: UIViewController {
         ])
     }
 }
-*/
+
+
+
+
+/*
+ class ViewController: UIViewController {
+ /*
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+  return 10
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt myIndexPath: IndexPath) -> UICollectionViewCell {
+  let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.myIndentifier, for: myIndexPath)
+  return myCell
+  }*/
+ //
+ private var myLayout = UICollectionViewFlowLayout()
+ //
+ private var myLabel = UILabel()
+ //
+ override func viewDidLoad() {
+ super.viewDidLoad()
+ 
+ view.backgroundColor = .red
+ myLayout = UICollectionViewFlowLayout()
+ myLayout.scrollDirection = .horizontal
+ myLayout.itemSize = CGSize(width: 67, height: 84)
+ myLayout.sectionInset = UIEdgeInsets(top: .zero, left: 12, bottom: 0, right: 12)
+ // Uma UICollectionView nao pode ser instanciada com o valor nulo
+ //Necessitando atribuir um valor ja nesse momento
+ let myCollectionView = UICollectionView(frame: .zero, collectionViewLayout: myLayout)
+ myCollectionView.translatesAutoresizingMaskIntoConstraints = false
+ myCollectionView.backgroundColor = .blue
+ //myCollectionView.dataSource = self
+ //myCollectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: MyCollectionViewCell.myIndentifier)
+ //
+ myLabel.translatesAutoresizingMaskIntoConstraints = false
+ myLabel.text = "My Label"
+ myLabel.textColor = .cyan
+ myLabel.font = UIFont.systemFont(ofSize: 10, weight: .semibold)
+ //
+ view.addSubview(myCollectionView)
+ 
+ NSLayoutConstraint.activate([
+ myCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 300),
+ myCollectionView.heightAnchor.constraint(equalToConstant: 84),
+ myCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+ myCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+ ])
+ }
+ 
+ 
+ }
+ */
