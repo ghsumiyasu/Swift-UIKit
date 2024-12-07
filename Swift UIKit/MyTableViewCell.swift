9f0814d814854b75e341a367eabceac619ac83fa
ghsumiyasu <ghsumiyasu@gmail.com>
2024-12-07 14:23:18 +0900

    UITableView na forma simplificada sem a variavel tipo lazy

--- Swift UIKit/MyTableViewCell.swift
+++ Swift UIKit/MyTableViewCell.swift
@@ -11,38 +11,30 @@ class MyTableViewCell: UITableViewCell {
 
     static let myIndentifier: String = "myTable"
     
-    private lazy var myLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Label"
-        label.textColor = .magenta
-        label.font = UIFont.systemFont(ofSize: 10, weight: .semibold)
-        return label
-    }()
-    
-    private lazy var myImage: UIImageView = {
-        let image = UIImageView()
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.image = UIImage(named: "myImage")
-        image.contentMode = .scaleAspectFit
-        return image
-    }()
-    
-    private lazy var myStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [myLabel, myImage])
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .horizontal
-        stackView.spacing = 4
-        stackView.layer.borderWidth = 1
-        stackView.layer.borderColor = UIColor.yellow.cgColor
-        return stackView
-    }()
-    
+    private var myLabel = UILabel()
+    private var myImage = UIImageView()
+    private var myStackView = UIStackView()
     //
-    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         backgroundColor = .green
+        
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        myLabel.text = "Label"
+        myLabel.textColor = .magenta
+        myLabel.font = UIFont.systemFont(ofSize: 10, weight: .semibold)
+        //
+        myImage.translatesAutoresizingMaskIntoConstraints = false
+        myImage.image = UIImage(named: "myImage")
+        myImage.contentMode = .scaleAspectFit
+        //
+        myStackView = UIStackView(arrangedSubviews: [myLabel, myImage])
+        myStackView.translatesAutoresizingMaskIntoConstraints = false
+        myStackView.axis = .horizontal
+        myStackView.spacing = 4
+        myStackView.layer.borderWidth = 1
+        myStackView.layer.borderColor = UIColor.yellow.cgColor
+        //
         contentView.addSubview(myStackView)
         NSLayoutConstraint.activate([
             myStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
@@ -57,3 +49,57 @@ class MyTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
+
+/*
+ class MyTableViewCell: UITableViewCell {
+
+     static let myIndentifier: String = "myTable"
+     
+     private lazy var myLabel: UILabel = {
+         let label = UILabel()
+         label.translatesAutoresizingMaskIntoConstraints = false
+         label.text = "Label"
+         label.textColor = .magenta
+         label.font = UIFont.systemFont(ofSize: 10, weight: .semibold)
+         return label
+     }()
+     
+     private lazy var myImage: UIImageView = {
+         let image = UIImageView()
+         image.translatesAutoresizingMaskIntoConstraints = false
+         image.image = UIImage(named: "myImage")
+         image.contentMode = .scaleAspectFit
+         return image
+     }()
+     
+     private lazy var myStackView: UIStackView = {
+         let stackView = UIStackView(arrangedSubviews: [myLabel, myImage])
+         stackView.translatesAutoresizingMaskIntoConstraints = false
+         stackView.axis = .horizontal
+         stackView.spacing = 4
+         stackView.layer.borderWidth = 1
+         stackView.layer.borderColor = UIColor.yellow.cgColor
+         return stackView
+     }()
+     
+     //
+     
+     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+         super.init(style: style, reuseIdentifier: reuseIdentifier)
+         backgroundColor = .green
+         contentView.addSubview(myStackView)
+         NSLayoutConstraint.activate([
+             myStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+             myStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+             myStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+             myStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+             myImage.heightAnchor.constraint(equalToConstant: 50)
+         ])
+     }
+     
+     required init?(coder: NSCoder) {
+         fatalError("init(coder:) has not been implemented")
+     }
+ }
+ */
--- Swift UIKit/ViewController.swift
+++ Swift UIKit/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
@@ -19,21 +20,17 @@ class ViewController: UIViewController, UITableViewDataSource {
         let myCell = tableView.dequeueReusableCell(withIdentifier: MyTableViewCell.myIndentifier, for: indexPath)
         return myCell
     }
+    private var myTableView = UITableView()
     
-    private lazy var myTableView:UITableView = {
-        let table = UITableView()
-        table.translatesAutoresizingMaskIntoConstraints = false
-        table.backgroundColor = .blue
-        table.dataSource = self
-        //O indentificador referenciado pela string "myIndentififier"
-        //table.register(MyTableViewCell.self, forCellReuseIdentifier: "myIndentifier")
-        table.register(MyTableViewCell.self, forCellReuseIdentifier: MyTableViewCell.myIndentifier)
-        return table
-    }()
-
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        
+        myTableView.translatesAutoresizingMaskIntoConstraints = false
+        myTableView.backgroundColor = .blue
+        myTableView.dataSource = self
+        myTableView.register(MyTableViewCell.self, forCellReuseIdentifier: MyTableViewCell.myIndentifier)
+        
         view.addSubview(myTableView)
         NSLayoutConstraint.activate([
             myTableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 500),
@@ -46,3 +43,44 @@ class ViewController: UIViewController, UITableViewDataSource {
 
 }
 
+
+/*
+ class ViewController: UIViewController, UITableViewDataSource {
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return 10
+     }
+     
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         //Forma de representar o indentificador atravez de uma string onde o mesmo deve ser
+         //refenciado na definicao da variavel instanciada da UITableView
+         //let myCell = tableView.dequeueReusableCell(withIdentifier: "myIndentifier", for: indexPath)
+         let myCell = tableView.dequeueReusableCell(withIdentifier: MyTableViewCell.myIndentifier, for: indexPath)
+         return myCell
+     }
+     
+     private lazy var myTableView:UITableView = {
+         let table = UITableView()
+         table.translatesAutoresizingMaskIntoConstraints = false
+         table.backgroundColor = .blue
+         table.dataSource = self
+         //O indentificador referenciado pela string "myIndentififier"
+         //table.register(MyTableViewCell.self, forCellReuseIdentifier: "myIndentifier")
+         table.register(MyTableViewCell.self, forCellReuseIdentifier: MyTableViewCell.myIndentifier)
+         return table
+     }()
+
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         view.backgroundColor = .red
+         view.addSubview(myTableView)
+         NSLayoutConstraint.activate([
+             myTableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 500),
+             myTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+             myTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+             myTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+         ])
+     }
+
+
+ }
+ */
